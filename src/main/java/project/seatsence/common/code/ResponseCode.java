8069cf069e8015094ed859f622071ed39ec9be10
benjaminuj <chosj1526@gmail.com>
2023-06-09 09:04:59 +0900

    chore: set common response code
    field and http response code setting

---
+++ src/main/java/project/seatsence/common/code/ResponseCode.java
@@ -0,0 +1,23 @@
+package project.seatsence.common.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+/** 에러 및 성공 코드 관리 */
+@Getter
+@AllArgsConstructor
+public enum ResponseCode {
+
+    /** 2xx : 성공 */
+    SUCCESS(OK, "200_OK", "요청에 성공하였습니다."),
+
+    /** 5xx : Server 오류 */
+    INTERNAL_ERROR(INTERNAL_SERVER_ERROR, "500_INTERNAL_SERVER_ERROR", "서버와의 연결에 실패하였습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
