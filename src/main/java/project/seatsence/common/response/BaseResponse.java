b19bce0d83e8c3b70ce236c5371ba9c7049bf0de
benjaminuj <chosj1526@gmail.com>
2023-06-09 09:05:35 +0900

    chore: set BaseResponse

---
+++ src/main/java/project/seatsence/common/response/BaseResponse.java
@@ -0,0 +1,34 @@
+package project.seatsence.common.response;
+
+import static project.seatsence.common.code.ResponseCode.SUCCESS;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonPropertyOrder({"isSuccess", "status", "code", "message", "data", "timestamp"})
+public class BaseResponse {
+    private final Boolean isSuccess = true;
+    private final int status;
+    private final String code;
+    private final String message;
+    private Object data;
+    private final String timestamp = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+
+    // 요청 성공
+    public BaseResponse(int status, Object data) {
+        this.message = SUCCESS.getMessage();
+        this.code = SUCCESS.getCode();
+        this.status = status;
+        this.data = data;
+    }
+}
+
