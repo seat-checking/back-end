96067e15239c1e2b9824d4f8aa46796f5ed35d5a
benjaminuj <chosj1526@gmail.com>
2023-06-09 21:32:59 +0900

    feat: Set user domain, user dto and user api
    user domain = User, UserRole, UserSex
    dto = ValidateEmailRequest, ValidateUserInformationResponse

---
+++ src/main/java/project/seatsence/src/user/api/UserApi.java
@@ -0,0 +1,33 @@
+package project.seatsence.src.user.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.seatsence.src.user.dto.request.ValidateEmailRequest;
+import project.seatsence.src.user.dto.response.ValidateUserInformationResponse;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/v1/users")
+@Tag(name = "01. [User]")
+@Validated
+@RequiredArgsConstructor
+public class UserApi {
+
+    private final UserSignUpService userSignUpService;
+
+    @Operation(summary = "이메일 검증 및 중복 확인")
+    @PostMapping("/validate/email")
+    public ValidateUserInformationResponse validateEmail(
+            @Valid @RequestBody ValidateEmailRequest validateEmailRequest) {
+        return userSignUpService.isEmailDuplicated(validateEmailRequest.getEmail());
+    }
+
+
+}
---
+++ src/main/java/project/seatsence/src/user/dto/response/ValidateUserInformationResponse.java
@@ -0,0 +1,12 @@
+package project.seatsence.src.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidateUserInformationResponse {
+    private Boolean isValid;
+}
