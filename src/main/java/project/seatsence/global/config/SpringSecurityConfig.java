7eaf6ac3c2270f5b91adbe2a98412dfe5f1a2d65
benjaminuj <chosj1526@gmail.com>
2023-06-16 15:17:33 +0900

    feat: Create JwtTokenProvider for generate Access Token

--- src/main/java/project/seatsence/global/config/SpringSecurityConfig.java
+++ src/main/java/project/seatsence/global/config/SpringSecurityConfig.java
@@ -1,21 +1,22 @@
 package project.seatsence.global.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.formLogin().disable().cors().and().csrf().disable()
                 .authorizeRequests().anyRequest().permitAll()
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .formLogin()
-                .disable();
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
     }
