d30bbb20110c6684bc86b5645e2a3be71512fc0e
benjaminuj <chosj1526@gmail.com>
2023-06-19 16:49:16 +0900

    fix: Solve conflict

--- src/main/java/project/seatsence/global/jwt/JwtTokenProvider.java
+++ src/main/java/project/seatsence/global/jwt/JwtTokenProvider.java
@@ -1,17 +1,37 @@
 package project.seatsence.global.jwt;
 
 import io.jsonwebtoken.*;
+
+import java.security.Key;
 import java.util.Date;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@RequiredArgsConstructor
 @Component
-public class JwtTokenProvider {
+public class JwtTokenProvider implements InitializingBean {
 
-    @Value("${JWT_SECRET_KEY}")
-    private String secretKey;
+    private final String secretKey;
+    private final Long accessTokenExp;
+
+    private Key key;
+
+    public JwtTokenProvider(
+            @Value("${JWT_SECRET_KEY}") String secretKey,
+            @Value("${JWT_ACCESS_EXP}") Long accessTokenExp) {
+        this.secretKey = secretKey;
+        this.accessTokenExp=accessTokenExp*1000;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
 
     public String generateAccessToken(Long id) {
         Date issuedAt = new Date();
