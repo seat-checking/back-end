ea68be901f606c03905453219a6ff8d13e5294e7
benjaminuj <chosj1526@gmail.com>
2023-07-06 13:25:52 +0900

    feat: Create RefreshToken entity

---
+++ src/main/java/project/seatsence/src/auth/domain/RefreshToken.java
@@ -0,0 +1,27 @@
+package project.seatsence.src.auth.domain;
+
+import javax.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.seatsence.src.user.domain.User;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "id")
+    private User user;
+
+    private String refreshToken;
+
+    public RefreshToken(User user, String refreshToken) {
+        this.user = user;
+        this.refreshToken = refreshToken;
+    }
+}
