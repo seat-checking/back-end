e10a2e9f12244c180f1b225ab18b5fabca6ac5f5
benjaminuj <chosj1526@gmail.com>
2023-07-17 19:27:39 +0900

    hotfix: Resolve circular references

--- src/main/java/project/seatsence/src/user/service/CustomUserDetailsService.java
+++ src/main/java/project/seatsence/src/user/service/CustomUserDetailsService.java
@@ -2,29 +2,28 @@ package project.seatsence.src.user.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import project.seatsence.global.code.ResponseCode;
 import project.seatsence.global.exceptions.BaseException;
+import project.seatsence.src.user.dao.UserRepository;
 import project.seatsence.src.user.domain.User;
 import project.seatsence.src.user.dto.CustomUserDetailsDto;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-    private final UserService userService;
-
-    public CustomUserDetailsService(UserService service) {
-        this.userService = service;
-    }
+    private final UserRepository userRepository;
 
     @Override
     public CustomUserDetailsDto loadUserByUsername(String username)
             throws UsernameNotFoundException {
-        User user = userService.findUserByUserEmail(username);
-        if (user == null) {
-            throw new BaseException(ResponseCode.USER_NOT_FOUND);
-        }
+        User user =
+                userRepository
+                        .findByEmail(username)
+                        .orElseThrow(() -> new BaseException(ResponseCode.USER_NOT_FOUND));
 
         List<String> roles = new ArrayList<>();
         roles.add(user.getRole().toString());
