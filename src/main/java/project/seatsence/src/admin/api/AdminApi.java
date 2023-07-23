4caac5913e476f24ea655161eefd94527b0d5361
0binn <binny9904@naver.com>
2023-07-23 16:00:04 +0900

    refactor: Apply Spotless

--- src/main/java/project/seatsence/src/admin/api/AdminApi.java
+++ src/main/java/project/seatsence/src/admin/api/AdminApi.java
@@ -13,7 +13,6 @@ import project.seatsence.src.admin.dto.request.AdminSignUpRequest;
 import project.seatsence.src.admin.dto.response.AdminNewBusinessInformationResponse;
 import project.seatsence.src.admin.dto.response.AdminSignInResponse;
 import project.seatsence.src.admin.service.AdminService;
-import project.seatsence.src.store.domain.StoreMember;
 import project.seatsence.src.store.service.StoreMemberService;
 import project.seatsence.src.user.domain.User;
 import project.seatsence.src.user.dto.CustomUserDetailsDto;
--- src/main/java/project/seatsence/src/admin/dto/response/AdminSignInResponse.java
+++ src/main/java/project/seatsence/src/admin/dto/response/AdminSignInResponse.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import project.seatsence.src.store.domain.StorePosition;
 
 @Getter
 @Setter
