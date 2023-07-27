4629620a3679dbf4fcb1f359f7118fec6300859c
benjaminuj <chosj1526@gmail.com>
2023-07-27 16:44:56 +0900

    fix: Change reservationDateAndType field type of SeatReservationRequest

--- src/main/java/project/seatsence/src/reservation/dto/request/SeatReservationRequest.java
+++ src/main/java/project/seatsence/src/reservation/dto/request/SeatReservationRequest.java
@@ -1,14 +1,13 @@
 package project.seatsence.src.reservation.dto.request;
 
-import java.time.LocalDateTime;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class SeatReservationRequest {
-    private Long storeChairId;
-    private Long userId;
+    @NotNull private Long storeChairId;
+    @NotNull private Long userId;
 
-    @NotNull private LocalDateTime reservationStartDateAndTime;
-    private LocalDateTime reservationEndDateAndTime;
+    @NotNull private String reservationStartDateAndTime;
+    @NotNull private String reservationEndDateAndTime;
 }
