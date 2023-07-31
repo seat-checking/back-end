4f8103e8929741df9a27087953ef93d1b474647a
benjaminuj <chosj1526@gmail.com>
2023-07-31 15:47:38 +0900

    enhancement: Add CANCELED enum value to ReservationStatus enum

--- src/main/java/project/seatsence/src/reservation/domain/ReservationStatus.java
+++ src/main/java/project/seatsence/src/reservation/domain/ReservationStatus.java
@@ -8,8 +8,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ReservationStatus {
     PENDING("PENDING", "대기"),
-    APPROVED("APPROVED", "승인"),
-    REJECTED("REJECTED", "거절");
+    CANCELED("CANCELED", "취소"), // 유저 직접 취소
+    APPROVED("APPROVED", "승인"), // 관리자 승인
+    REJECTED("REJECTED", "거절"); // 관리자 거절
 
     private String value;
 
