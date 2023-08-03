4b14064fd08c052c5886877d1fe414a090abdd7b
0binn <binny9904@naver.com>
2023-08-03 12:31:00 +0900

    refactor: apply spotless

--- src/main/java/project/seatsence/src/reservation/api/AdminReservationApi.java
+++ src/main/java/project/seatsence/src/reservation/api/AdminReservationApi.java
@@ -17,12 +17,12 @@ public class AdminReservationApi {
 
     private final AdminReservationService adminReservationService;
 
-    @Operation(summary = "admin 예약 상태 변경하기",
-            description = "예약 상태는 '승인', '거절' 중 하나로 선택")
+    @Operation(summary = "admin 예약 상태 변경하기", description = "예약 상태는 '승인', '거절' 중 하나로 선택")
     @PostMapping("/{store-id}/")
-    public void setReservationStatus(@PathVariable("store-id") Long storeId,
-                                     @RequestParam("reservation-id") Long reservationId,
-                                     @RequestBody ReservationStatus reservationStatus){
+    public void setReservationStatus(
+            @PathVariable("store-id") Long storeId,
+            @RequestParam("reservation-id") Long reservationId,
+            @RequestBody ReservationStatus reservationStatus) {
         adminReservationService.setReservationStatus(reservationId, reservationStatus);
     }
 }
--- src/main/java/project/seatsence/src/reservation/service/AdminReservationService.java
+++ src/main/java/project/seatsence/src/reservation/service/AdminReservationService.java
@@ -1,5 +1,7 @@
 package project.seatsence.src.reservation.service;
 
+import static project.seatsence.global.code.ResponseCode.RESERVATION_NOT_FOUND;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,8 +10,6 @@ import project.seatsence.src.reservation.dao.ReservationRepository;
 import project.seatsence.src.reservation.domain.Reservation;
 import project.seatsence.src.reservation.domain.ReservationStatus;
 
-import static project.seatsence.global.code.ResponseCode.RESERVATION_NOT_FOUND;
-
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -17,13 +17,13 @@ public class AdminReservationService {
 
     private final ReservationRepository reservationRepository;
 
-    public Reservation findById(Long id){
+    public Reservation findById(Long id) {
         return reservationRepository
                 .findById(id)
                 .orElseThrow(() -> new BaseException(RESERVATION_NOT_FOUND));
     }
 
-    public void setReservationStatus(Long reservationId, ReservationStatus reservationStatus){
+    public void setReservationStatus(Long reservationId, ReservationStatus reservationStatus) {
         Reservation reservation = findById(reservationId);
         reservation.setReservationStatus(reservationStatus);
     }
