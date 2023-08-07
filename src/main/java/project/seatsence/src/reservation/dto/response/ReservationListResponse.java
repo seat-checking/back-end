b9e196198da96dfc28f5047a1eec1320f41a8ceb
0binn <binny9904@naver.com>
2023-08-07 21:07:39 +0900

    fix: delete setter

--- src/main/java/project/seatsence/src/reservation/dto/response/ReservationListResponse.java
+++ src/main/java/project/seatsence/src/reservation/dto/response/ReservationListResponse.java
@@ -6,18 +6,15 @@ import javax.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import project.seatsence.src.reservation.domain.ReservationStatus;
 
 @Getter
-@Setter
 @AllArgsConstructor
 @Builder
 public class ReservationListResponse {
     private List<ReservationResponse> reservationResponseList;
 
     @Getter
-    @Setter
     @AllArgsConstructor
     @Builder
     public static class ReservationResponse {
