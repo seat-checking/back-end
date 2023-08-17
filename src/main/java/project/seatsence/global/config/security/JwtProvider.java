ce6bb2a87d7435707c0071626eee19468b0074ba
benjaminuj <chosj1526@gmail.com>
2023-08-17 16:32:08 +0900

    fix: Change token expired date to client test

--- src/main/java/project/seatsence/global/config/security/JwtProvider.java
+++ src/main/java/project/seatsence/global/config/security/JwtProvider.java
@@ -112,7 +112,7 @@ public class JwtProvider implements InitializingBean {
     private static Date createAccessTokenExpiredDate() {
         Calendar issuedAt = Calendar.getInstance();
         //        issuedAt.add(Calendar.MINUTE, 30); // 30분
-        issuedAt.add(Calendar.MINUTE, 1); // test용 // Todo : 테스트 끝나면, 실제 만료시간으로 바꾸기
+        issuedAt.add(Calendar.MINUTE, 5); // test용 // Todo : 테스트 끝나면, 실제 만료시간으로 바꾸기
 
         return issuedAt.getTime();
     }
@@ -124,8 +124,8 @@ public class JwtProvider implements InitializingBean {
      */
     private static Date createRefreshTokenExpiredDate() {
         Calendar issuedAt = Calendar.getInstance();
-        //        issuedAt.add(Calendar.DAY_OF_MONTH, 2); // 2주
-        issuedAt.add(Calendar.MINUTE, 2); // test용 // Todo : 테스트 끝나면, 실제 만료시간으로 바꾸기
+        issuedAt.add(Calendar.DAY_OF_MONTH, 14); // 2주
+        //        issuedAt.add(Calendar.MINUTE, 30); // test용 // Todo : 테스트 끝나면, 실제 만료시간으로 바꾸기
 
         return issuedAt.getTime();
     }
