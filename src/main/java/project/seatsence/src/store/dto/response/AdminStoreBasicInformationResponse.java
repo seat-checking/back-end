59c6998d2087c2f56f4a96383e09758f354ea3a0
Yerim Hyun <hyunyelim25@gmail.com>
2023-08-25 20:13:04 +0900

    feat: separate admin store information API
    - basic information API
    - operating time API

---
+++ src/main/java/project/seatsence/src/store/dto/response/AdminStoreBasicInformationResponse.java
@@ -0,0 +1,31 @@
+package project.seatsence.src.store.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.seatsence.src.store.domain.Store;
+
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Getter
+public class AdminStoreBasicInformationResponse {
+    private String storeName;
+    private String address;
+    private String detailAddress;
+    private String category;
+    private String mainImage; // TODO 메인 이미지 및 이미지 리스트 전송
+    private String introduction;
+
+    public static AdminStoreBasicInformationResponse of(Store store) {
+        return AdminStoreBasicInformationResponse.builder()
+                .storeName(store.getStoreName())
+                .address(store.getAddress())
+                .detailAddress(store.getDetailAddress())
+                .category(store.getCategory().getValue())
+                .mainImage(store.getMainImage())
+                .introduction(store.getIntroduction())
+                .build();
+    }
+}
