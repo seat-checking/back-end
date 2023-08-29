6dcd784c7116f808ba52f400662e1e92fe0de72b
benjaminuj <chosj1526@gmail.com>
2023-08-29 14:59:00 +0900

    feat: Create findByIdAndState method to WalkInService class

--- src/main/java/project/seatsence/src/utilization/service/walkin/WalkInService.java
+++ src/main/java/project/seatsence/src/utilization/service/walkin/WalkInService.java
@@ -1,10 +1,12 @@
 package project.seatsence.src.utilization.service.walkin;
 
 import static project.seatsence.global.entity.BaseTimeAndStateEntity.State.*;
+import static project.seatsence.global.code.ResponseCode.*;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import project.seatsence.global.exceptions.BaseException;
 import project.seatsence.src.utilization.dao.walkin.WalkInRepository;
 import project.seatsence.src.utilization.domain.walkin.WalkIn;
@@ -23,6 +25,6 @@ public class WalkInService {
 
     public WalkIn findByIdAndState(Long id) {
         return walkInRepository.findByIdAndState(id, ACTIVE)
-                .orElseThrow(() -> new BaseException())
+                .orElseThrow(() -> new BaseException(WALK_IN_NOT_FOUND));
     }
 }
