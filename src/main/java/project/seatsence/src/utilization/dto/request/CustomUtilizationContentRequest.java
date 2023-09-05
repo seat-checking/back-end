1056151055eb342a30b2860d3270706ac67ae92f
0binn <binny9904@naver.com>
2023-09-05 17:03:35 +0900

    fix: fix typo

--- src/main/java/project/seatsence/src/utilization/dto/request/CustomUtilizationContentRequest.java
+++ src/main/java/project/seatsence/src/utilization/dto/request/CustomUtilizationContentRequest.java
@@ -9,6 +9,6 @@ public class CustomUtilizationContentRequest {
 
     @NotNull private Long fieldId;
 
-    @NotBlank(message = "이용 신청 정보 내용이이 입력지 않았습니다.")
+    @NotBlank(message = "이용 신청 정보 내용이 입력되지 않았습니다.")
     private String content;
 }
