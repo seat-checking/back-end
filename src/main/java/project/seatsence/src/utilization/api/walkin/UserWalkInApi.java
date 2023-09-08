40d717ae7c915cb72e5182668c38ead7e8cf8a8e
0binn <binny9904@naver.com>
2023-09-08 16:51:06 +0900

    feat: user walk-in list api

--- src/main/java/project/seatsence/src/utilization/api/walkin/UserWalkInApi.java
+++ src/main/java/project/seatsence/src/utilization/api/walkin/UserWalkInApi.java
@@ -7,11 +7,19 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.api.annotations.ParameterObject;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import project.seatsence.global.config.security.JwtProvider;
+import project.seatsence.global.response.SliceResponse;
+import project.seatsence.src.utilization.domain.walkin.WalkIn;
 import project.seatsence.src.utilization.dto.request.ChairUtilizationRequest;
 import project.seatsence.src.utilization.dto.request.SpaceUtilizationRequest;
+import project.seatsence.src.utilization.dto.response.reservation.AdminReservationListResponse;
+import project.seatsence.src.utilization.dto.response.walkin.UserWalkInListResponse;
 import project.seatsence.src.utilization.service.walkin.UserWalkInService;
 
 @RestController
@@ -44,5 +52,21 @@ public class UserWalkInApi {
         String userEmail = JwtProvider.getUserEmailFromValidToken(accessToken, refreshToken);
 
         userWalkInService.inputSpaceWalkIn(userEmail, spaceUtilizationRequest);
+    }
+
+    @Operation(summary = "유저 바로 사용 리스트")
+    @GetMapping("/my-list")
+    public SliceResponse<UserWalkInListResponse.WalkInResponse> getUserWalkInList(
+            @RequestHeader(AUTHORIZATION_HEADER) String accessToken,
+            @CookieValue(COOKIE_NAME_PREFIX_SECURE + REFRESH_TOKEN_NAME) String refreshToken,
+            @ParameterObject @PageableDefault(page = 1, size = 15) Pageable pageable){
+
+        String userEmail = JwtProvider.getUserEmailFromValidToken(accessToken, refreshToken);
+        Slice<WalkIn> walkInSlice = userWalkInService.getAllWalkIn(userEmail,pageable);
+
+        SliceResponse<UserWalkInListResponse.WalkInResponse> sliceResponse =
+                userWalkInService.toSliceResponse(walkInSlice);
+
+        return sliceResponse;
     }
 }
--- src/main/java/project/seatsence/src/utilization/dao/walkin/WalkInRepository.java
+++ src/main/java/project/seatsence/src/utilization/dao/walkin/WalkInRepository.java
@@ -1,9 +1,13 @@
 package project.seatsence.src.utilization.dao.walkin;
 
 import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.seatsence.global.entity.BaseTimeAndStateEntity.*;
+import project.seatsence.src.user.domain.User;
 import project.seatsence.src.utilization.domain.walkin.WalkIn;
 
 @Repository
@@ -11,4 +15,6 @@ public interface WalkInRepository extends JpaRepository<WalkIn, Long> {
     WalkIn save(WalkIn walkIn);
 
     Optional<WalkIn> findByIdAndState(Long id, State state);
+
+    Slice<WalkIn> findAllByUserEmailAndStateOrderByStartScheduleDesc(String Email, State state, Pageable pageable);
 }
