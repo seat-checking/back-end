58a6d3d1391b56737c98f02ca4cd5e4f871d1d47
0binn <binny9904@naver.com>
2023-10-10 14:48:13 +0900

    fix: add NotNull message

--- src/main/java/project/seatsence/src/utilization/dao/participation/ParticipationRepository.java
+++ src/main/java/project/seatsence/src/utilization/dao/participation/ParticipationRepository.java
@@ -4,14 +4,11 @@ import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import project.seatsence.global.entity.BaseTimeAndStateEntity;
 import project.seatsence.src.user.domain.User;
 import project.seatsence.src.utilization.domain.Participation.Participation;
 import project.seatsence.src.utilization.domain.Participation.ParticipationStatus;
 import project.seatsence.src.utilization.domain.reservation.Reservation;
-import project.seatsence.src.utilization.domain.walkin.WalkIn;
 
 public interface ParticipationRepository
         extends JpaRepository<Participation, Long>, ParticipationRepositoryCustom {
@@ -26,11 +23,4 @@ public interface ParticipationRepository
 
     Boolean existsByUserAndReservationAndState(
             User user, Reservation reservation, BaseTimeAndStateEntity.State state);
-
-    @Query(
-            "SELECT p FROM Participation p WHERE p.user = :user AND p.walkIn = :walkIn AND p.state = :state")
-    Boolean findByUserAndWalkInAndState(
-            @Param("user") User user,
-            @Param("walkIn") WalkIn walkIn,
-            @Param("state") BaseTimeAndStateEntity.State state);
 }
